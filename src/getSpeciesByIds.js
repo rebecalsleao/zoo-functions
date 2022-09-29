01401d33e99a0a039110853101938d49571fc0c6
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-09-29 20:18:19 -0300

    Projeto - Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,22 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) {
+    return [];
+  }
+  if (ids.length === 1) {
+    return [data.species.find((specie) => specie.id === ids[0])];
+  }
+  if (ids.length > 1) {
+    const listSpecies = [];
+
+    ids.forEach((id) => {
+      const specie = data.species.find((specieZoo) => specieZoo.id === id);
+      listSpecies.push(specie);
+    });
+
+    return listSpecies;
+  }
 }
 
 module.exports = getSpeciesByIds;
