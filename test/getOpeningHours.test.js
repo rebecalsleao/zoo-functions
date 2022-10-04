c959c5dc5181c93da5055c8a1e6516ace6e240e1
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-10-04 18:49:49 -0300

    Projeto - Requisito 12

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,25 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Verificar se não passando argumentos retorna o objeto', () => {
+    const week = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
+    expect(getOpeningHours()).toMatchObject(week);
+  });
+  it('Verificar se para os argumentos Monday e 09:00-AM deve retornar a string "The zoo is closed"', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toEqual('The zoo is closed');
+  });
+  it('Verificar para os argumentos Tuesday e 09:00-AM deve retornar a string "The zoo is open"', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toEqual('The zoo is open');
+  });
+  it('Verificar se para os argumentos Wednesday e 09:00-PM deve retornar a string "The zoo is closed"', () => {
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toEqual('The zoo is closed');
+  });
+});
