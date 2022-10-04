944277da3cf270ac90c8744808a41781526515a6
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-10-04 10:29:01 -0300

    Projeto - Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,32 @@
 const data = require('../data/zoo_data');
 
+// const animal = { specie: 'penguins' }
+
 function countAnimals(animal) {
-  // seu código aqui
+  const result = {};
+  if (animal === undefined) {
+    data.species.forEach((element) => {
+      const keySpecies = element.name;
+      const valueSpecies = element.residents.length;
+      result[keySpecies] = valueSpecies;
+    });
+    return result;
+  } else if (animal.sex === undefined) {
+    return data.species.find((specie) => specie.name === animal.specie).residents.length;
+  } else {
+    return data.species.find((specie) => specie.name === animal.specie).residents.filter((resident) => resident.sex === animal.sex).length;
+  }
 }
 
 module.exports = countAnimals;
+
+// let result;
+// data.species.forEach((element) => {
+//   if (element.name === specie) {
+//     result = element.residents.every((resident) => resident.age >= minAge);
+//   }
+// });
+// return result;
+// }
+
+// Object.value(parametro).length === 0
