fddb0fc901af3b7f4e3138cd7ecaa549c14c7c7b
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-10-04 19:32:00 -0300

    Projeto  - Requisito 13

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -22,4 +22,16 @@ describe('Testes da função getOpeningHours', () => {
   it('Verificar se para os argumentos Wednesday e 09:00-PM deve retornar a string "The zoo is closed"', () => {
     expect(getOpeningHours('Wednesday', '09:00-PM')).toEqual('The zoo is closed');
   });
+  it('Verificar para os argumentos Thu e 09:00-AM se deve lançar uma exceção com mensagem específica', () => {
+    expect(() => getOpeningHours('Thu', '09:00-AM')).toThrowError(new Error('The day must be valid. Example: Monday'));
+  });
+  it('Verificar se para os argumentos Friday e 09:00-ZM se deve lançar uma exceção com mensagem específica', () => {
+    expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrowError(new Error('The abbreviation must be "AM" or "PM"'));
+  });
+  it('Verificar para os argumentos Saturday e C9:00-AM se deve lançar uma exceção com a mensagem específica', () => {
+    expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrowError(new Error('The hour should represent a number'));
+  });
+  it('Para os argumentos Sunday e 09:c0-AM deve lançar uma exceção com a mensagem específica', () => {
+    expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrowError(new Error('The minutes should represent a number'));
+  });
 });
