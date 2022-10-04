610549d968a3049a4bc38971f95ebaaac6e11ff0
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-10-04 17:36:58 -0300

    Projeto - Requisito 7

--- src/handlerElephants.js
+++ src/handlerElephants.js
@@ -1,21 +1,19 @@
 const { species } = require('../data/zoo_data');
 
-const getElephants = () =>
-  species.find((specie) => specie.name === 'elephants');
+const getElephants = () => species.find((specie) => specie.name === 'elephants');
 
-const averageAge = ({ residents }) =>
-  residents.reduce((sum, elephant) => sum + elephant.age, 0) / residents.length;
+const averageAge = ({ residents }) => residents.reduce((sum, elephant) => sum + elephant.age, 0) / residents.length;
 
 const computeData = (param, elephants) => {
   switch (param) {
-  case 'count':
-    return elephants.residents.length;
-  case 'names':
-    return elephants.residents.map((elephant) => elephant.name);
-  case 'averageAge':
-    return averageAge(elephants);
-  default:
-    return null;
+    case 'count':
+      return elephants.residents.length;
+    case 'names':
+      return elephants.residents.map((elephant) => elephant.name);
+    case 'averageAge':
+      return averageAge(elephants);
+    default:
+      return null;
   }
 };
 
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -10,4 +10,17 @@ describe('Testes da função HandlerElephants', () => {
   it('Verificar se o argumento averageAge deve retornar um número próximo a 10.5', () => {
     expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
   });
+  it('Verificar se o argumento location retornar a string NW', () => {
+    expect(handlerElephants('location')).toEqual('NW');
+  });
+  it('Verificar se o argumento popularity retornar um número igual ou maior a 5', () => {
+    expect(handlerElephants('popularity')).toBeCloseTo(5);
+  });
+  it('Verificar se o argumento availability deve retornar um array de dias da semana que não contém Monday', () => {
+    const expected = ['Monday'];
+    expect(handlerElephants('availability')).toEqual(expect.not.arrayContaining(expected));
+  });
+  it('Verificar se não passando argumentos a função retorna undefined', () => {
+    expect(handlerElephants()).toBe(undefined);
+  });
 });
