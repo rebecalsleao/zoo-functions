5316c123eaf9e6773508c53cb53cbe2deea0896d
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-10-07 20:58:57 -0300

    Projeto - Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employeeFound = data.employees.find((employee) => employee.id === id);
+  const specieId = employeeFound.responsibleFor[0];
+  const specieFound = data.species.find((spacie) => spacie.id === specieId);
+  // eslint-disable-next-line max-len
+  const resident = specieFound.residents.reduce((prev, current) => (prev.age > current.age ? prev : current));
+  return Object.values(resident);
 }
 
 module.exports = getOldestFromFirstSpecies;
