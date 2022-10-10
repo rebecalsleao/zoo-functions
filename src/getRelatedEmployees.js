cae3a34cbcaa118834074618b49d64b8bac0d2b6
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-10-09 22:01:30 -0300

    Projeto - Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,31 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const listManagers = [];
+  data.employees.forEach((employee) => {
+    employee.managers.forEach((managerId) => {
+      listManagers.push(managerId);
+    });
+  });
+  const managerFound = listManagers.find((manager) => manager === id);
+  if (managerFound === undefined) {
+    return false;
+  }
+  return true;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    const listEmployees = [];
+    data.employees.forEach((employee) => {
+      const managerFound = employee.managers.find((manager) => managerId === manager);
+      if (managerFound !== undefined) {
+        listEmployees.push(`${employee.firstName} ${employee.lastName}`);
+      }
+    });
+    return listEmployees;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
